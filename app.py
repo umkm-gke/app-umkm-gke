f6d4e79f96a8beb855a4497f9ed275c173243b8b
umkm-gke <umkmgadingkirana@gmail.com>
2025-06-27 21:46:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -492,14 +492,14 @@ if st.session_state.role == 'guest' and menu_selection == "Belanja":
                             if pd.notnull(last_updated):
                                 last_updated = last_updated.to_pydatetime()
                             is_new = pd.notnull(last_updated) and (datetime.datetime.now() - last_updated).days <= 14
-                            st.write({
+                            
+                        except Exception:
+                            is_new = False
+                        st.write({
                             "product": product['product_name'],
                             "last_updated": last_updated,
                             "is_new": is_new
-                            })
-                        except Exception:
-                            is_new = False
-                        
+                        })
                         new_badge = " <span style='color:green; font-size:0.9em;'>🆕 Produk Baru</span>" if is_new else ""
                         product_title = f"<div class='custom-title'>{product['product_name'][:30]}{new_badge}</div>"
                         st.markdown(product_title, unsafe_allow_html=True)
