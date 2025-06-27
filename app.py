f613809dce3ca44303a10ddd15d78c427c240250
umkm-gke <umkmgadingkirana@gmail.com>
2025-06-28 01:11:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -386,14 +386,22 @@ with st.sidebar:
 # ========================
 query_params = st.query_params
 url_vendor = query_params.get("vendor")
+# Siapkan vendor list dan default vendor
+active_products['vendor_name'] = active_products['vendor_name'].str.strip()
+vendor_list = sorted([str(v) for v in active_products['vendor_name'].dropna().unique()])
 
+default_vendor = "Semua"
+disable_vendor_select = False
 if st.session_state.role == 'guest' and menu_selection == "Belanja":
-    if url_vendor and default_vendor != "Semua":
-        judul = f"Katalog Produk {default_vendor}"
-        subjudul = f"_Temukan produk terbaik dari **{default_vendor}**_"
-    else:
-        judul = "Katalog Produk"
-        subjudul = "_Temukan produk terbaik dari UMKM GKE_"
+    if url_vendor:
+        url_vendor = url_vendor.strip()
+        for v in vendor_list:
+            if v.lower() == url_vendor.lower():
+                default_vendor = v
+                disable_vendor_select = True
+                break
+    judul = "Katalog Produk"
+    subjudul = "_Temukan produk terbaik dari UMKM GKE_"
     
     st.markdown(f"### <img src='https://cdn-icons-png.flaticon.com/512/1170/1170678.png' width='25'/> {judul}", unsafe_allow_html=True)
     st.markdown("_Temukan produk terbaik dari UMKM GKE_")
